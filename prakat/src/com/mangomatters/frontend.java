6cab4c9cdeb5c1c14f9f39750f82793e72098af1
menanmavericks <menanmavericks@gmail.com>
2019-03-14 12:33:35 +0530

    stage 1

---
+++ prakat/src/com/mangomatters/frontend.java
@@ -0,0 +1,11 @@
+package com.mangomatters;
+
+public class frontend {
+
+	public static void main(String[] args) {
+		System.out.println("heloo alllll");
+		// TODO Auto-generated method stub
+
+	}
+
+}
